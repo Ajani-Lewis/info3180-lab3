665ca75f73c3f17852ba6ab95cc1d226544af149
Ajani-Lewis <ajani.j.lewis@gmail.com>
2022-02-20 12:56:53 -0500

    Wrote code to send mail

--- app/views.py
+++ app/views.py
@@ -8,6 +8,8 @@ This file creates your application.
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 from .forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 
 ###
@@ -37,6 +39,10 @@ def contact():
             email = cform.email.data
             subject = cform.subject.data
             msg = cform.msg.data
+
+            smtp_msg = Message(subject, sender=(name,email),recipients=["to@example.com"])
+            smtp_msg.body = msg
+            mail.send(smtp_msg)
 
     return render_template('contact.html',form=cform)
 
