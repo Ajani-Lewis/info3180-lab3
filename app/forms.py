7adddbfb978ebe6b8a3e3f47dc341dabaee17550
Ajani-Lewis <ajani.j.lewis@gmail.com>
2022-02-20 12:18:19 -0500

    Edited form page

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,10 @@
-from tkinter import Widget
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email
-#from wtforms.widgets import TextArea
+from wtforms.widgets import TextArea
 
 class ContactForm(FlaskForm):
     name = StringField('Full Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     subject = StringField('Subject', validators=[DataRequired()])
-    msg = StringField('Message', validators=[DataRequired()])
+    msg = StringField('Message', validators=[DataRequired()], widget=TextArea())
